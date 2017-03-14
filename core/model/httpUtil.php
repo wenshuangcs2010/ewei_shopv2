c4236f53f0cad4a66e2dd9e9858d802a854e4898
happykress <wenshuangcs2008@live.com>
2017-03-14 14:43:24 +0800

    保税商品数据同步
    保税商品数据同步

---
+++ core/model/httpUtil.php
@@ -0,0 +1,95 @@
+<?php
+
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+
+class HttpUtil_EweiShopV2Model
+{
+
+	//获取保税超市库存
+	function getGoods($goodssn){
+		load()->func('communication');
+		$goodsurl="http://www.cnbuyers.cn/index.php?app=webService&act=checkStockWe7&sku=".$goodssn;
+		$resp = ihttp_get($goodsurl);
+		 $content = $resp['content'];
+	      if (empty($content)) {
+	            return array();
+	      }
+	      $content=(array)@json_decode($content);
+	      $data=(array)$content['data'];
+	      return $data['stock'];
+	}
+	//更新库存 
+	function updateStock($id,$stock){
+		$goods = pdo_fetch("select * from " . tablename('ewei_shop_goods') . " where id=:id and uniacid=:uniacid limit 1", array(':id' => $id, ':uniacid' => DIS_ACCOUNT));
+		$sql="select id from " . tablename('ewei_shop_goods') . " where disgoods_id=:disgoods_id";
+		$disgoodslist=pdo_fetchall($sql,array("disgoods_id"=>$goods['id']));
+		
+		foreach ($disgoodslist as $v) {
+			pdo_update("ewei_shop_goods",array("total"=>$stock),array("id"=>$v));
+		}
+		
+	}
+
+	//更新保税超市库存
+	function updateGoods($goodssn,$goodsid){
+		load()->func('communication');
+		$goodsurl="http://www.cnbuyers.cn/index.php?app=webService&act=We7GetGoodsInfo&sku=".$goodssn;
+		$resp = ihttp_get($goodsurl);
+		$content = $resp['content'];
+		if (empty($content)) {
+	            return array();
+	      }
+	    $content=(array)@json_decode($content);
+	    $data=(array)$content['data'];
+	    if(empty($data)){return array();}
+	    $updatedata=array(
+	    	'title'=>$data['goods_name'],
+	    	'total'=>$data['stock'],
+	    	'weight'=>$data['weight']*1000,
+	    	'consumption_tax'=>$data['consumption_tax'],
+	    	'vat_rate'=>$data['vat_rate'],
+	    	'tariffnum'=>$data['hs_code'],
+	    	'unit'=>$data['unit'],
+	    	'minbuy'=>$data['min_quantity'],
+	    
+	    	);
+	    var_Dump($data['sku']);
+	    m("order")->updatestock($data['only_sku'],120);
+	    die();
+	    pdo_update("ewei_shop_goods",$updatedata,array("id"=>$goodsid));
+	    $sql="select id from " . tablename('ewei_shop_goods') . " where disgoods_id=:disgoods_id";
+	    $disgoodslist=pdo_fetchall($sql,array("disgoods_id"=>$goodsid));
+	    $disdata=array(
+			'total'=>$data['stock'],
+	    	'weight'=>$data['weight']*1000,
+	    	'consumption_tax'=>$data['consumption_tax'],
+	    	'vat_rate'=>$data['vat_rate'],
+	    	'tariffnum'=>$data['hs_code'],
+	    	'unit'=>$data['unit'],
+	    	'minbuy'=>$data['min_quantity'],
+	    	);
+	    $sql="update ".tablename("ewei_shop_goods")." SET ";
+	    foreach($disdata as $k=>$v){
+	    	if(is_numeric($v)){
+	    		$str.="`".$k."`=".$v.",";
+	    	}elseif(is_string($v)){
+	    		$str.="`".$k.'`="'.$v.'",';
+	    	}
+	    	
+	    }
+	    $str=substr($str, 0, -1);
+	    $sql.=$str;
+	    foreach ($disgoodslist as $v) {
+		   $t[]=$v['id'];
+		}
+		$ids=implode(",",$t);
+		$sql.=" where id in ($ids)";
+		pdo_query($sql);
+		return 1;
+	}
+
+
+
+}
---
+++ core/web/http/httpdown.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('IN_IA')) {
+	exit('Access Denied');
+}
+
+class Httpdown_EweiShopV2Page extends WebPage {
+
+	function main(){
+		global $_W, $_GPC;
+		$id = intval($_GPC['id']);
+		$goods = pdo_fetch("select * from " . tablename('ewei_shop_goods') . " where id=:id and uniacid=:uniacid limit 1", array(':id' => $id, ':uniacid' => $_W['uniacid']));
+		//var_dump($goods);
+		$return=m("httpUtil")->updateGoods($goods['goodssn'],$goods['id']);
+		if($return){
+			show_json(1,"更新成功");
+		}
+		show_json(0,'更新失败');
+	}
+}
