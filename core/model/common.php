4d982cf700285e61ff3422ce0af007ec1906c24f
happykress <wenshuangcs2008@live.com>
2017-03-15 14:22:40 +0800

    海关订单号不能超过18位
    海关订单号不能超过18位

--- core/model/common.php
+++ core/model/common.php
@@ -1081,13 +1081,13 @@ class Common_EweiShopV2Model {
 	//生成单号
 	public function createNO($table, $field, $prefix) {
 
-		$billno = date('YmdHis') . random(6, true);
+		$billno = date('YmdHis') . random(2, true);
 		while (1) {
 			$count = pdo_fetchcolumn('select count(*) from ' . tablename('ewei_shop_' . $table) . " where {$field}=:billno limit 1", array(':billno' => $billno));
 			if ($count <= 0) {
 				break;
 			}
-			$billno = date('YmdHis') . random(6, true);
+			$billno = date('YmdHis') . random(2, true);
 		}
 		return $prefix . $billno;
 	}
